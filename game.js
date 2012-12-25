136042044fbbb8ee05bd14dae8d7cc30a4f7ec00
Rafał Gałczyński <rafalgalczynski@gmail.com>
2012-12-25 21:34:20 +0100

    fixed typo?

--- game.js
+++ game.js
@@ -1,49 +1,49 @@
 if (typeof(Game) == 'undefined')
 {
 
-(function() {
+    (function() {
 
-// namespaces
-var Game = window.Game = {};
+	 // namespaces
+	 var Game = window.Game = {};
 
-var Direction = Game.Direction =
-{
-    UpperLeft:  0,
-    UpperRight: 1,
-    LowerLeft:  2,
-    LowerRight: 3
-};
+	 var Direction = Game.Direction =
+	     {
+		 UpperLeft:  0,
+		 UpperRight: 1,
+		 LowerLeft:  2,
+		 LowerRight: 3
+	     };
 
-Game.State = function State()
-{
-    this.type = 'A';
-    this.playerDirection = Direction.UpperLeft;
-    this.generatedCount  = 0;
-    this.droppedCount    = 0;
-    this.supperLeftRail = [false, false, false, false, false];
-    this.upperRightRail = [false, false, false, false, false];
-    this.lowerLeftRail  = [false, false, false, false, false];
-    this.lowerRightRail = [false, false, false, false, false];
-    this.leftEscape     = [false, false, false, false, false];
-    this.rightEscape    = [false, false, false, false, false];
-};
+	 Game.State = function State()
+	 {
+	     this.type = 'A';
+	     this.playerDirection = Direction.UpperLeft;
+	     this.generatedCount  = 0;
+	     this.droppedCount    = 0;
+	     this.upperLeftRail =  [false, false, false, false, false];
+	     this.upperRightRail = [false, false, false, false, false];
+	     this.lowerLeftRail  = [false, false, false, false, false];
+	     this.lowerRightRail = [false, false, false, false, false];
+	     this.leftEscape     = [false, false, false, false, false];
+	     this.rightEscape    = [false, false, false, false, false];
+	 };
 
-Game.onStateChange = function(callback)
-{
-};
+	 Game.onStateChange = function(callback)
+	 {
+	 };
 
-Game.startGameA = function()
-{
-};
+	 Game.startGameA = function()
+	 {
+	 };
 
-Game.startGameB = function()
-{
-};
+	 Game.startGameB = function()
+	 {
+	 };
 
-Game.setDirection = function(dir)
-{
-};
+	 Game.setDirection = function(dir)
+	 {
+	 };
 
-})();
+     })();
 
 }
