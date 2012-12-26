1334ca84e9db69069e021de3fba0a778a89cc30e
Rafał Gałczyński <rafalgalczynski@gmail.com>
2012-12-26 18:44:10 +0100

    added "better" way of getting mouse coords...

--- radom.js
+++ radom.js
@@ -206,10 +206,10 @@ function gameOnClick(event) {
 	return false;
     };
     var surface = document.querySelector("#game");
-    var context = surface.getContext("2d");
+    var coords = surface.relMouseCoords(event);
 
-    var x = event.offsetX;
-    var y = event.offsetY;
+    var x = coords.x;
+    var y = coords.y;
     if(x.between(437, 456) && y.between(32, 51)) {
         Game.startGameA();
     }
@@ -287,3 +287,24 @@ function init()
 {
     initAssets();
 }
+
+
+function relMouseCoords(event){
+    var totalOffsetX = 0;
+    var totalOffsetY = 0;
+    var canvasX = 0;
+    var canvasY = 0;
+    var currentElement = this;
+    
+    do{
+        totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
+        totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
+    }
+    while(currentElement = currentElement.offsetParent)
+    
+    canvasX = event.pageX - totalOffsetX;
+    canvasY = event.pageY - totalOffsetY;
+    
+    return {x:canvasX, y:canvasY}
+}
+HTMLCanvasElement.prototype.relMouseCoords = relMouseCoords;
